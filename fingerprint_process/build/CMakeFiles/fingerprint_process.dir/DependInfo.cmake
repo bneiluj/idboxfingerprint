ab21a91c24b87e1adc03a9e05d791ac38628f764
bneiluj <julien.bouteloup@gmail.com>
2017-07-16 11:34:31 +0100

    Add cpp libs

---
+++ fingerprint_process/build/CMakeFiles/fingerprint_process.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/j.bouteloup/Projects/IDbox/AndroidApp/openCV/OpenCVBlueprints/chapter_6/source_code/fingerprint/fingerprint_process/fingerprint_process.cpp" "/Users/j.bouteloup/Projects/IDbox/AndroidApp/openCV/OpenCVBlueprints/chapter_6/source_code/fingerprint/fingerprint_process/build/CMakeFiles/fingerprint_process.dir/fingerprint_process.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ fingerprint_process/build/CMakeFiles/fingerprint_process.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fingerprint_process.dir/fingerprint_process.cpp.o"
+  "fingerprint_process.pdb"
+  "fingerprint_process"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fingerprint_process.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
